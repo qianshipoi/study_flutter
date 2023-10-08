7bf8bc23e80be73c4e964c44f5d0402d8b9dc3ed
未来 栗山 <2410128587@qq.com>
2023-10-08 22:27:07 +0800

    complate

--- lib/main.dart
+++ lib/main.dart
@@ -55,32 +55,69 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Row(
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          body: Row(
+        children: [
+          SafeArea(
+              child: NavigationRail(
+            extended: constraints.maxWidth >= 600,
+            destinations: [
+              NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text("Home")),
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite),
+                label: Text('Favorites'),
+              )
+            ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+          )),
+          Expanded(
+              child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ))
+        ],
+      ));
+    });
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('No favorites yet'));
+    }
+
+    return ListView(
       children: [
-        SafeArea(
-            child: NavigationRail(
-          extended: false,
-          destinations: [
-            NavigationRailDestination(
-                icon: Icon(Icons.home), label: Text("Home")),
-            NavigationRailDestination(
-              icon: Icon(Icons.favorite),
-              label: Text('Favorites'),
-            )
-          ],
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (value) {
-            print(value);
-          },
-        )),
-        Expanded(
-            child: Container(
-          color: Theme.of(context).colorScheme.primaryContainer,
-          child: GeneratorPage(),
-        ))
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(leading: Icon(Icons.favorite), title: Text(pair.asLowerCase))
       ],
-    ));
+    );
   }
 }
 
